3f1e65a759dfeab64d8c53c757e54aa0beca07e4
Justin Taing <justinxuetaing@gmail.com>
2024-07-01 17:55:03 -0700

    Added org bar nav bar and sidebar

---
+++ app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,13 @@
+"use client"
+import { UserButton } from "@clerk/clerk-react"
+
+export const Navbar = () => {
+    return (
+        <div className = "flex items-center gap-x-4 p-5 bg-green-500">
+            <div className = "hidden lg:flex lg:flex-1">
+                {/* Search bar*/}
+            </div>
+            <UserButton/>
+        </div>
+    );
+};
---
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import {Plus} from "lucide-react"
+import { CreateOrganization } from "@clerk/nextjs"
+
+import {
+    Dialog,
+    DialogContent,
+    DialogTrigger,
+}   from "@/components/ui/dialog";
+
+export const NewButton  = () => {
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <div>
+                    <button>
+                        < Plus className = "text-white" />
+                    </button>
+                </div>
+            </DialogTrigger>
+        </Dialog>
+    )
+}
